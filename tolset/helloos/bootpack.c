6f5b12b5920d4d82bb170df3a44c5cb27e476dc8
tang fuhao <fangshiyu2@gmail.com>
2014-09-11 19:43:00 +0800

    3

--- tolset/helloos/bootpack.c
+++ tolset/helloos/bootpack.c
@@ -1,251 +1,31 @@
-// #include <stdio.h>
+/* bootpack‚ÌƒƒCƒ“ */
 
-void io_hlt(void);
-void io_cli(void);
-void io_out8(int port,int data);
-int  io_load_eflags(void);
-void io_store_eflags(int eflags);
-
-//åˆå§‹åŒ–è°ƒè‰²æ¿
-void init_palette(void);
-//è®¾ç½®è°ƒè‰²æ¿
-void set_palette(int start,int end, unsigned char *rgb);
-//ç”»çŸ©å½¢
-void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
-//åˆ›å»ºçŸ©å½¢æ¡Œé¢
-void init_screen8(char *vram, int x, int y);
-//æ˜¾ç¤ºä¸€ä¸ªå­—ç¬¦
-void putfont8(char* vram, int xsize, int x, int y, char c, char* font);
-//æ˜¾ç¤ºå­—ç¬¦ä¸² asciiç¼–ç 
-void putfonts8_asc(char* vram, int xsize, int x, int y, char c,unsigned char* s);
-//åˆå§‹åŒ–é¼ æ ‡æ•°æ®
-void init_mouse_cursor8(char *mouse, char bc);
-//
-void putblock8_8(char *vram, int vxsize, int pxsize,int pysize, int px0, int py0, char *buf, int bxsize);
-
-void init_gdtidt(void);
-void set_segmdesc(SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
-void set_gatedesc(GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
-
-#define COL8_000000		0
-#define COL8_FF0000		1
-#define COL8_00FF00		2
-#define COL8_FFFF00		3
-#define COL8_0000FF		4
-#define COL8_FF00FF		5
-#define COL8_00FFFF		6
-#define COL8_FFFFFF		7
-#define COL8_C6C6C6		8
-#define COL8_840000		9
-#define COL8_008400		10
-#define COL8_848400		11
-#define COL8_000084		12
-#define COL8_840084		13
-#define COL8_008484		14
-#define COL8_848484		15
-
-typedef struct _BOOTINFO
-{
-	char cyls, leds, vmode, reserve;
-	short scrnx, scrny;
-	char* vram;
-}BOOTINFO;
-
-typedef struct _SEGMENT_DESCRIPTOR
-{
-	short limit_low,base_low;
-	char base_mid,access_mid;
-	char limit_high,base_high;
-}SEGMENT_DESCRIPTOR;
-
-typedef struct _GATE_DESCRIPTOR
-{
-	short offset_low,selector;
-	char dw_count,access_right;
-	short offset_high;
-}GATE_DESCRIPTOR;
+#include "bootpack.h"
 
 
 void HariMain(void)
 {
-	BOOTINFO *binfo = (BOOTINFO*)0x0ff0;
+	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
 	char s[40], mcursor[256];
 	int mx, my;
 
+	init_gdtidt();
+	init_pic();
+	io_sti(); /* IDT/PIC‚Ì‰Šú‰»‚ªI‚í‚Á‚½‚Ì‚ÅCPU‚ÌŠ„‚è‚İ‹Ö~‚ğ‰ğœ */
 
 	init_palette();
-	init_screen8(binfo->vram,binfo->scrnx,binfo->scrny);
-
-	putfonts8_asc(binfo->vram,binfo->scrnx,8,8,COL8_FFFFFF,"Hello World!");
-
-	mx = (binfo->scrnx - 16) / 2; 
+	init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
+	mx = (binfo->scrnx - 16) / 2; /* ‰æ–Ê’†‰›‚É‚È‚é‚æ‚¤‚ÉÀ•WŒvZ */
 	my = (binfo->scrny - 28 - 16) / 2;
 	init_mouse_cursor8(mcursor, COL8_008484);
 	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
-
 	// sprintf(s, "(%d, %d)", mx, my);
 	// putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
-	fin:
+	io_out8(PIC0_IMR, 0xf9); /* PIC1‚ÆƒL[ƒ{[ƒh‚ğ‹–‰Â(11111001) */
+	io_out8(PIC1_IMR, 0xef); /* ƒ}ƒEƒX‚ğ‹–‰Â(11101111) */
+
+	for (;;) {
 		io_hlt();
-		goto fin;
-}
-
-void init_palette(void){
-	static unsigned char table_rgb[16 * 3] = {
-		0x00,0x00,0x00,	/* 0:é»‘ */
-		0xff,0x00,0x00,	/* 1:äº®çº¢ */
-		0x00,0xff,0x00,	/* 2:äº®ç»¿ */
-		0xff,0xff,0x00,	/* 3:äº®é»„ */
-		0x00,0x00,0xff,	/* 4:äº®è“ */
-		0xff,0x00,0xff,	/* 5:äº®ç´« */
-		0x00,0xff,0xff,	/* 6:æµ…äº®è“ */
-		0xff,0xff,0xff,	/* 7:ç™½ */
-		0xc6,0xc6,0xc6,	/* 8:äº®ç° */
-		0x84,0x00,0x00,	/* 9:æš—çº¢ */
-		0x00,0x84,0x00,	/* 10:æš—ç»¿ */
-		0x84,0x84,0x00,	/* 11:æš—é»„ */
-		0x00,0x00,0x84,	/* 12:æš—é’ */
-		0x84,0x00,0x84,	/* 13:æš—ç´« */
-		0x00,0x84,0x84,	/* 14:æµ…æš—è“ */
-		0x84,0x84,0x84	/* 15:æš—ç° */
-	};
-	set_palette(0,15,table_rgb);
-	return ;
-}
-
-void set_palette(int start,int end, unsigned char *rgb){
-	int i,eflags;
-	eflags = io_load_eflags();	/*è®°å½•ä¸­æ–­è®¸å¯æ ‡å¿—çš„å€¼*/
-	io_cli();	/*å°†ä¸­æ–­è®¸å¯æ ‡å¿—ç½®ä¸º0ï¼Œç¦æ­¢ä¸­æ–­*/
-	io_out8(0x03c9,start);
-	for (i = start; i <= end; i++)
-	{
-		io_out8(0x03c9,rgb[0] / 4);
-		io_out8(0x03c9,rgb[1] / 4);
-		io_out8(0x03c9,rgb[2] / 4);
-		rgb += 3;
 	}
-	return ;
-}
-
-void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1){
-	int x,y;
-	for (y = y0; y <= y1; y++)
-	{
-		for (x = x0; x <= x1; x++)
-			vram[y * xsize + x] = c;
-	}
-	return ;
-}
-
-void init_screen8(char *vram, int x, int y)
-{
-	boxfill8(vram, x, COL8_008484,  0,     0,      x -  1, y - 29);
-	boxfill8(vram, x, COL8_C6C6C6,  0,     y - 28, x -  1, y - 28);
-	boxfill8(vram, x, COL8_FFFFFF,  0,     y - 27, x -  1, y - 27);
-	boxfill8(vram, x, COL8_C6C6C6,  0,     y - 26, x -  1, y -  1);
-
-	boxfill8(vram, x, COL8_FFFFFF,  3,     y - 24, 59,     y - 24);
-	boxfill8(vram, x, COL8_FFFFFF,  2,     y - 24,  2,     y -  4);
-	boxfill8(vram, x, COL8_848484,  3,     y -  4, 59,     y -  4);
-	boxfill8(vram, x, COL8_848484, 59,     y - 23, 59,     y -  5);
-	boxfill8(vram, x, COL8_000000,  2,     y -  3, 59,     y -  3);
-	boxfill8(vram, x, COL8_000000, 60,     y - 24, 60,     y -  3);
-
-	boxfill8(vram, x, COL8_848484, x - 47, y - 24, x -  4, y - 24);
-	boxfill8(vram, x, COL8_848484, x - 47, y - 23, x - 47, y -  4);
-	boxfill8(vram, x, COL8_FFFFFF, x - 47, y -  3, x -  4, y -  3);
-	boxfill8(vram, x, COL8_FFFFFF, x -  3, y - 24, x -  3, y -  3);
-	return;
-}
-
-void putfont8(char* vram, int xsize, int x, int y, char c, char* font){
-	int i;
-	char* p;
-	char data;
-	for (i = 0; i < 16; ++i)
-	{
-		p = vram + (y+i) * xsize + x;
-		data = font[i];
-		if ((data & 0x80) != 0)	{p[0] = c; }
-		if ((data & 0x40) != 0)	{p[1] = c; }
-		if ((data & 0x20) != 0)	{p[2] = c; }
-		if ((data & 0x10) != 0)	{p[3] = c; }
-		if ((data & 0x08) != 0)	{p[4] = c; }
-		if ((data & 0x04) != 0)	{p[5] = c; }
-		if ((data & 0x02) != 0)	{p[6] = c; }
-		if ((data & 0x01) != 0)	{p[7] = c; }
-	}
-	return ;
-}
-
-void putfonts8_asc(char* vram, int xsize, int x, int y, char c,unsigned char* s){
-	extern char hankaku[4096];
-	for (; *s != 0; ++s)
-	{
-		putfont8(vram,xsize,x,y,c,hankaku + *s * 16);
-		x += 8;
-	}
-}
-
-void init_mouse_cursor8(char *mouse, char bc)
-{
-	static char cursor[16][16] = {
-		"**************..",
-		"*OOOOOOOOOOO*...",
-		"*OOOOOOOOOO*....",
-		"*OOOOOOOOO*.....",
-		"*OOOOOOOO*......",
-		"*OOOOOOO*.......",
-		"*OOOOOOO*.......",
-		"*OOOOOOOO*......",
-		"*OOOO**OOO*.....",
-		"*OOO*..*OOO*....",
-		"*OO*....*OOO*...",
-		"*O*......*OOO*..",
-		"**........*OOO*.",
-		"*..........*OOO*",
-		"............*OO*",
-		".............***"
-	};
-	int x, y;
-
-	for (y = 0; y < 16; y++) {
-		for (x = 0; x < 16; x++) {
-			if (cursor[y][x] == '*') {
-				mouse[y * 16 + x] = COL8_000000;
-			}
-			if (cursor[y][x] == 'O') {
-				mouse[y * 16 + x] = COL8_FFFFFF;
-			}
-			if (cursor[y][x] == '.') {
-				mouse[y * 16 + x] = bc;
-			}
-		}
-	}
-	return;
-}
-
-void putblock8_8(char *vram, int vxsize, int pxsize,
-	int pysize, int px0, int py0, char *buf, int bxsize)
-{
-	int x, y;
-	for (y = 0; y < pysize; y++) {
-		for (x = 0; x < pxsize; x++) {
-			vram[(py0 + y) * vxsize + (px0 + x)] = buf[y * bxsize + x];
-		}
-	}
-	return;
-}
-
-
-void init_gdtidt(void){
-
-}
-void set_segmdesc(SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar){
-
-}
-void set_gatedesc(GATE_DESCRIPTOR *gd, int offset, int selector, int ar){
-	
 }
--- tolset/helloos/ipl10.nas
+++ tolset/helloos/ipl10.nas
@@ -1,97 +1,105 @@
-CYLS	EQU		10
-ORG		0x7c00
-JMP		entry
-DB		0x90
-DB		"HARIBOTE"		
-DW		512
-DB		1
-DW		1
-DB		2
-DW		224
-DW		2880			
-DB		0xf0			
-DW		9
-DW		18
-DW		2
-DD		0
-DD		2880			
-DB		0,0,0x29		
-DD		0xffffffff		
-DB		"HARIBOTEOS "	
-DB		"FAT12   "		
-RESB	18
+; haribote-ipl
+; TAB=4
+
+CYLS	EQU		10				; どこまで読み込むか
+
+		ORG		0x7c00			; このプログラムがどこに読み込まれるのか
+
+; 以下は標準的なFAT12フォーマットフロッピーディスクのための記述
+
+		JMP		entry
+		DB		0x90
+		DB		"HARIBOTE"		; ブートセクタの名前を自由に書いてよい（8バイト）
+		DW		512				; 1セクタの大きさ（512にしなければいけない）
+		DB		1				; クラスタの大きさ（1セクタにしなければいけない）
+		DW		1				; FATがどこから始まるか（普通は1セクタ目からにする）
+		DB		2				; FATの個数（2にしなければいけない）
+		DW		224				; ルートディレクトリ領域の大きさ（普通は224エントリにする）
+		DW		2880			; このドライブの大きさ（2880セクタにしなければいけない）
+		DB		0xf0			; メディアのタイプ（0xf0にしなければいけない）
+		DW		9				; FAT領域の長さ（9セクタにしなければいけない）
+		DW		18				; 1トラックにいくつのセクタがあるか（18にしなければいけない）
+		DW		2				; ヘッドの数（2にしなければいけない）
+		DD		0				; パーティションを使ってないのでここは必ず0
+		DD		2880			; このドライブ大きさをもう一度書く
+		DB		0,0,0x29		; よくわからないけどこの値にしておくといいらしい
+		DD		0xffffffff		; たぶんボリュームシリアル番号
+		DB		"HARIBOTEOS "	; ディスクの名前（11バイト）
+		DB		"FAT12   "		; フォーマットの名前（8バイト）
+		RESB	18				; とりあえず18バイトあけておく
+
+; プログラム本体
 
 entry:
-MOV		AX,0			
-MOV		SS,AX
-MOV		SP,0x7c00
-MOV		DS,AX
+		MOV		AX,0			; レジスタ初期化
+		MOV		SS,AX
+		MOV		SP,0x7c00
+		MOV		DS,AX
 
-MOV		AX,0x0820
-MOV		ES,AX
-MOV		CH,0			
-MOV		DH,0			
-MOV		CL,2			
+; ディスクを読む
+
+		MOV		AX,0x0820
+		MOV		ES,AX
+		MOV		CH,0			; シリンダ0
+		MOV		DH,0			; ヘッド0
+		MOV		CL,2			; セクタ2
 readloop:
-MOV		SI,0			
-
+		MOV		SI,0			; 失敗回数を数えるレジスタ
 retry:
-MOV		AH,0x02			
-MOV		AL,1			
-MOV		BX,0
-MOV		DL,0x00			
-INT		0x13			
-JNC		next			
-ADD		SI,1			
-CMP		SI,5			
-JAE		error			
-MOV		AH,0x00
-MOV		DL,0x00			
-INT		0x13			
-JMP		retry
-
+		MOV		AH,0x02			; AH=0x02 : ディスク読み込み
+		MOV		AL,1			; 1セクタ
+		MOV		BX,0
+		MOV		DL,0x00			; Aドライブ
+		INT		0x13			; ディスクBIOS呼び出し
+		JNC		next			; エラーがおきなければnextへ
+		ADD		SI,1			; SIに1を足す
+		CMP		SI,5			; SIと5を比較
+		JAE		error			; SI >= 5 だったらerrorへ
+		MOV		AH,0x00
+		MOV		DL,0x00			; Aドライブ
+		INT		0x13			; ドライブのリセット
+		JMP		retry
 next:
-MOV		AX,ES			
-ADD		AX,0x0020
-MOV		ES,AX			
-ADD		CL,1			
-CMP		CL,18			
-JBE		readloop		
-MOV		CL,1
-ADD		DH,1
-CMP		DH,2
-JB		readloop		
-MOV		DH,0
-ADD		CH,1
-CMP		CH,CYLS
-JB		readloop		
+		MOV		AX,ES			; アドレスを0x200進める
+		ADD		AX,0x0020
+		MOV		ES,AX			; ADD ES,0x020 という命令がないのでこうしている
+		ADD		CL,1			; CLに1を足す
+		CMP		CL,18			; CLと18を比較
+		JBE		readloop		; CL <= 18 だったらreadloopへ
+		MOV		CL,1
+		ADD		DH,1
+		CMP		DH,2
+		JB		readloop		; DH < 2 だったらreadloopへ
+		MOV		DH,0
+		ADD		CH,1
+		CMP		CH,CYLS
+		JB		readloop		; CH < CYLS だったらreadloopへ
 
-MOV		[0x0ff0],CH		
-JMP		0xc200
+; 読み終わったのでharibote.sysを実行だ！
+
+		MOV		[0x0ff0],CH		; IPLがどこまで読んだのかをメモ
+		JMP		0xc200
 
 error:
-MOV		SI,msg
-
+		MOV		SI,msg
 putloop:
-MOV		AL,[SI]
-ADD		SI,1			
-CMP		AL,0
-JE		fin
-MOV		AH,0x0e			
-MOV		BX,15			
-INT		0x10			
-JMP		putloop
-
+		MOV		AL,[SI]
+		ADD		SI,1			; SIに1を足す
+		CMP		AL,0
+		JE		fin
+		MOV		AH,0x0e			; 一文字表示ファンクション
+		MOV		BX,15			; カラーコード
+		INT		0x10			; ビデオBIOS呼び出し
+		JMP		putloop
 fin:
-HLT		
-JMP		fin
-
+		HLT						; 何かあるまでCPUを停止させる
+		JMP		fin				; 無限ループ
 msg:
-DB		0x0a, 0x0a		
-DB		"load error"
-DB		0x0a			
-DB		0
+		DB		0x0a, 0x0a		; 改行を2つ
+		DB		"load error"
+		DB		0x0a			; 改行
+		DB		0
 
-RESB	0x7dfe-$		
+		RESB	0x7dfe-$		; 0x7dfeまでを0x00で埋める命令
 
-DB		0x55, 0xaa
+		DB		0x55, 0xaa
